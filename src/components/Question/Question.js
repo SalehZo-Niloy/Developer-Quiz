8252e8c5acff92d3bb55b0df2dd043c5a46ab7a9
SalehZo-Niloy <msnniloy7@gmail.com>
2022-10-11 21:37:32 +0600

    Quiz answer gives write or wrong toast

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import OptionCard from '../OptionCard/OptionCard';
+import toast, { Toaster } from 'react-hot-toast';
 
 const Question = ({ ques }) => {
-    const { question, options, id } = ques;
+    const { question, options, id, correctAnswer } = ques;
     const formatQuestion = question.split('<p>').join('').split('</p>').join('');
     // console.log(formatQuestion);
+    const notify = (answer) => {
+        if (correctAnswer.toLowerCase() === answer.toLowerCase()) {
+            return toast.success('Your answer in Correct');
+        }
+        else {
+            return toast.error('Wrong answer, try again!!')
+        }
+    }
     // console.log(ques);
     return (
         <div className=' rounded-lg w-11/12 md:w-1/2 mx-auto my-6 bg-white'>
@@ -19,8 +28,9 @@ const Question = ({ ques }) => {
             <h4 className='font-medium'>Options are:</h4>
             <form className='grid grid-cols-1 md:grid-cols-2 gap-2 p-3'>
                 {
-                    options.map((option, idx) => <OptionCard key={id + idx} option={option} id={id + idx}></OptionCard>)
+                    options.map((option, idx) => <OptionCard key={id + idx} option={option} id={id + idx} notify={notify}></OptionCard>)
                 }
+                <Toaster />
             </form>
         </div>
     );
